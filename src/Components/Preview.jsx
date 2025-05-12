b5d059e89dd526822439a442f5fb647f00fe04ea
Ayantola Abisola Monsurat <ayantolamonsurat@gmail.com>
2025-05-12 23:01:35 +0100

    Deleted some components

--- src/Components/Preview.jsx
+++ src/Components/Preview.jsx
@@ -1,16 +1,16 @@
-import React, { useState } from 'react';
-import './CSS/previre.css';
-import html2canvas from 'html2canvas';
-import  html2pdf  from 'html2pdf.js';
-import { FaPhoneAlt,  FaLinkedinIn } from "react-icons/fa"
-import { FaLocationDot } from "react-icons/fa6"
-import { IoGlobeSharp } from "react-icons/io5"
-import { MdEmail } from "react-icons/md"
-import image from "../assets/download (1).png"
-import QRCode from 'react-qr-code';
+import React, { useState } from "react";
+import "./CSS/previre.css";
+import html2canvas from "html2canvas";
+import html2pdf from "html2pdf.js";
+import { FaPhoneAlt, FaLinkedinIn } from "react-icons/fa";
+import { FaLocationDot } from "react-icons/fa6";
+import { IoGlobeSharp } from "react-icons/io5";
+import { MdEmail } from "react-icons/md";
+import image from "../assets/download (1).png";
+import QRCode from "react-qr-code";
 
-function Preview({ formData}) {
-  const [viewQRcode, setViewQRcode] = useState(false)
+function Preview({ formData }) {
+  const [viewQRcode, setViewQRcode] = useState(false);
   const ExportAsPNG = async () => {
     try {
       const element = document.querySelector(".preview-container-wrapper");
@@ -33,33 +33,36 @@ function Preview({ formData}) {
   };
 
   const ExportAsPdf = async () => {
-   try {
-    const element = document.querySelector(".preview-container-wrapper");
-    element.style.display = 'block';
-   await html2pdf()
-     .set({
-       margin: 0,
-       filename: `${formData.displayName || "Business Card"}.pdf`,
-       image: { type: "jpeg", quality: 0.98 },
-       html2canvas: { scale: 3, useCORS: true },
-       jsPDF: { unit: "mm", format: "a4", orientation: "portrait" }
-     })
-     .from(element)
-     .save();
-   } catch (error) {
-    console.log(error)
-   }
+    try {
+      const element = document.querySelector(".preview-container-wrapper");
+      element.style.display = "block";
+      await html2pdf()
+        .set({
+          margin: 0,
+          filename: `${formData.displayName || "Business Card"}.pdf`,
+          image: { type: "jpeg", quality: 0.98 },
+          html2canvas: { scale: 3, useCORS: true },
+          jsPDF: { unit: "mm", format: "a4", orientation: "portrait" },
+        })
+        .from(element)
+        .save();
+    } catch (error) {
+      console.log(error);
+    }
   };
 
   return (
-    <div className='preview'>
+    <div className="preview">
       <h3>Card Preview</h3>
       <div className="preview-container-wrapper">
         <div className="preview-container">
           <div className="card-into">
             <div className="name-and-title">
               <div className="card-profile-photo">
-                <img src={formData.profilePhotoURL || image} alt="profile photo" />
+                <img
+                  src={formData.profilePhotoURL || image}
+                  alt="profile photo"
+                />
               </div>
               <div className="profile-info">
                 <h3>{formData.displayName || "Display Name"}</h3>
@@ -67,24 +70,40 @@ function Preview({ formData}) {
                 <p>{formData.role || "Your Role"}</p>
               </div>
               <div className="contact">
-                <p><FaPhoneAlt  className='FaPhone'/>{formData.number || "Phone Number"}</p>
-                <p><FaLocationDot  className='address'/>{formData.address || "Your Address"}</p>
+                <p>
+                  <FaPhoneAlt className="FaPhone" />
+                  {formData.number || "Phone Number"}
+                </p>
+                <p>
+                  <FaLocationDot className="address" />
+                  {formData.address || "Your Address"}
+                </p>
               </div>
             </div>
             <div className="right-side">
-              {/* <a href={formData.websiteURL}><QRCode value={formData.websiteURL} size={150}/></a> */}
+              <a href={formData.websiteURL}>
+                <QRCode value={formData.websiteURL} size={150} />
+              </a>
               <div className="socials">
-                <a  href={formData.linkedlnURL} className='linkedin'><FaLinkedinIn className='FaLinkedinIn'/></a>
-                <a href={formData.email} className='mail'><MdEmail className='MdEmail' /></a>
+                <a href={formData.linkedlnURL} className="linkedin">
+                  <FaLinkedinIn className="FaLinkedinIn" />
+                </a>
+                <a href={formData.email} className="mail">
+                  <MdEmail className="MdEmail" />
+                </a>
               </div>
             </div>
           </div>
         </div>
       </div>
       <div className="preview-btns">
-        <button className='Generate-QR-code' onClick={ExportAsPdf}>Export as Pdf</button>
-        <button className="download" onClick={ExportAsPNG}>Export as PNG</button>
-      </div>      
+        <button className="Generate-QR-code" onClick={ExportAsPdf}>
+          Export as Pdf
+        </button>
+        <button className="download" onClick={ExportAsPNG}>
+          Export as PNG
+        </button>
+      </div>
     </div>
   );
 }
